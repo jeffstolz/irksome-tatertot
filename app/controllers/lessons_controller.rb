0d8c7ce31af8ebd32d01a5338e8c259f4819d0e5
jeffstolzdesign <jeffstolzdesign@gmail.com>
2015-10-11 18:14:55 +0000

    enrollment fix, require enrollments for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,22 @@
 class LessonsController < ApplicationController
-  def show
-  end
+	before_action :authenticate_user!
+	before_action :require_course_enrollment_for_lessons, :only => [:show]
 
-  private
+	def show
+	end
+
+  	private
+
+  	def require_course_enrollment_for_lessons
+  		the_course = current_lesson.section.course
+
+    	if !current_lesson.section.course.user.enrolled_in?(the_course)
+    		redirect_to course_path(the_course), alert: 'Please enroll to access lessons'
+      end
+	end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
-  end  
+  end
 end
